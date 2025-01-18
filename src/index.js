411fbce1e3613966030cce1e4aaab149e4015939
김다영 <dayeong0120.com>
2025-01-18 19:44:52 +0900

    Docs : swagger 설정 및 프리즈마 모델 추가

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import dotenv from "dotenv";
 import express from 'express';          // -> ES Module
 import cors from "cors";
+import swaggerAutogen from "swagger-autogen";
+import swaggerUiExpress from "swagger-ui-express";
 
 dotenv.config();
 
@@ -12,6 +14,57 @@ app.use(cors());                            // cors 방식 허용
 app.use(express.static('public'));          // 정적 파일 접근
 app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
+
+
+app.use(
+  "/docs",
+  swaggerUiExpress.serve,
+  swaggerUiExpress.setup({}, {
+    swaggerOptions: {
+      url: "/openapi.json",
+    },
+  })
+);
+
+app.get("/openapi.json", async (req, res, next) => {
+  // #swagger.ignore = true
+  const options = {
+    openapi: "3.0.0",
+    disableLogs: true,
+    writeOutputFile: false,
+  };
+  const outputFile = "/dev/null"; // 파일 출력은 사용하지 않습니다.
+  const routes = ["./src/index.js"];
+  const doc = {
+    info: {
+      title: "team17",
+      description: "team17 테스트 문서서",
+    },
+    host: "localhost:3000",
+    components: {
+      securitySchemes: {
+        OAuth2: {
+          type: 'oauth2',
+          flows: {
+            authorizationCode: {
+              authorizationUrl: 'http://localhost:3000/oauth2/login/google',
+              tokenUrl: 'http://localhost:3000/oauth2/callback/google',
+              scopes: {
+                read: 'Grants read access',
+                write: 'Grants write access',
+                admin: 'Grants access to admin operations'
+              }
+            }
+          }
+        }
+      }
+    }
+  };
+
+  const result = await swaggerAutogen(options)(outputFile, routes, doc);
+  res.json(result ? result.data : null);
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
